263010949049890e02ee05afc816ed0fb7e3db5d
Oydual3 <claucondor@gmail.com>
2023-11-19 02:02:36 -0400

    register handler done

---
+++ pages/funciones/registro_handler.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre_usuario = htmlspecialchars($_POST['nombre_usuario']);
+    $contrasena = password_hash($_POST['contrasena'], PASSWORD_DEFAULT); 
+
+
+    agregarUsuario($nombre_usuario, $contrasena);
+
+    $_SESSION['mensaje'] = 'Registro exitoso. Ahora puedes iniciar sesión.';
+    header("Location: /proyecto/login.php");
+    exit();
+} else {
+    header("Location: /proyecto/index.php");
+    exit();
+}
+
+function agregarUsuario($nombre_usuario, $contrasena) {
+    $conexion = mysqli_connect("localhost", "carlos", "1234", "turismo_colombiano");
+
+    $nombre_usuario = mysqli_real_escape_string($conexion, $nombre_usuario);
+
+    $consulta = "INSERT INTO usuarios (nombre_usuario, contrasena) VALUES ('$nombre_usuario', '$contrasena')";
+    $resultado = mysqli_query($conexion, $consulta);
+
+    if (!$resultado) {
+        $_SESSION['error'] = 'Error al registrar el usuario. Por favor, inténtalo de nuevo.';
+    }
+    mysqli_close($conexion);
+}
+?>
